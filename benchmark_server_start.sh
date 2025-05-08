c51246fc8370733645c1f7d694f1c4573cb92e04
Jonas <jonasx.dufek@gmail.com>
2025-05-08 20:45:24 +0200

    Update benchmark_server_start.sh

--- benchmark_server_start.sh
+++ benchmark_server_start.sh
@@ -2,13 +2,10 @@
 sudo chmod -R 777 .
 sudo python3 ./server/frameworks/build_all_images.py
 
-alias spip='sudo $(printenv VIRTUAL_ENV)/bin/pip3'
-alias spython='sudo $(printenv VIRTUAL_ENV)/bin/python3'
-
 cd ./server/src/
 
 sudo python3 -m venv ./venv
 source venv/bin/activate
 
-sudo spip install -r requirements.txt
-sudo spython ./main.py
+sudo $(printenv VIRTUAL_ENV)/bin/pip3 install -r requirements.txt
+sudo $(printenv VIRTUAL_ENV)/bin/python3 ./main.py
